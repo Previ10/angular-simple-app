bb3e10b89f0ac819c49540395cdac12c5b1dc957
Ignacio Previgliano <ignaprevi@gmail.com>
2024-10-01 15:52:38 -0300

    agregamos cambios actualizados

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -6,20 +6,26 @@ import { MovieForm } from '../../models/formModel';
   selector: 'app-movie-card',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './movie-card.component.html', 
+  templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.css'],
 })
 export class MovieCardComponent {
-  @Input() pelicula!: MovieForm; 
+  @Input() pelicula!: MovieForm;
   @Input() index!: number;
   @Output() toggleFavorita = new EventEmitter<number>();
   @Output() deleteMovie = new EventEmitter<number>();
 
+  defaultImageUrl: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAWWV_TzALxkkrkC-4yhP7_2DTYAa7N0cABg&s';
 
   onToggleFavorita() {
     this.toggleFavorita.emit(this.index);
   }
-  onDeleteMovie(){
+
+  onDeleteMovie() {
     this.deleteMovie.emit(this.index);
+  }
+
+  onImageError() {
+    this.pelicula.imageUrl = this.defaultImageUrl;
   }
 }
